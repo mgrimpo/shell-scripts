ce928968f24b27592c0018229f6f1af3e3716636
Marc Grimpo <marc.grimpo@posteo.de>
2020-05-24 19:59:40 +0200

    break script into functions

--- move-to-own-folder.sh
+++ move-to-own-folder.sh
@@ -10,11 +10,12 @@
 
 
 
-# default values
-
+# Globals
+readonly PROGNAME=$(basename $0)
+readonly PROGDIR=$(greadlink -m $(dirname $0))
+readonly ARGS="$@"
 # the character that separates the folder name from the rest of the file name
 SEP='.'
-
 # whether or not to include the separator character at the end of the folder name
 INCLUDE_SEP=false
 
@@ -22,65 +23,110 @@ ERROR_OCURRED=false
 
 LOG_FILE="move-to-own-folder.sh.log"
 
+OTHER_ARGUMENTS=()
+
 # parsing arguments / flags
 # following https://pretzelhands.com/posts/command-line-flags
-OTHER_ARGUMENTS=()
-for arg in "$@"
-do
-    case $arg in
-        -n|--dry-run)
-            DRY_RUN=true
-            shift # Remove -n / --dry-run from positional arguments
-            ;;
-        -s=*|--sep=*)
-            SEP="${arg#*=}"
-            shift
-            ;;
-        -i|--include-sep)
-            INCLUDE_SEP=true
-            shift
-            ;;
-        *)
-            OTHER_ARGUMENTS+=("$1")
-    esac
-done
+parse_arguments(){
+    for arg in "$@"
+    do
+        case $arg in
+            -n|--dry-run)
+                DRY_RUN=true
+                shift # Remove -n / --dry-run from positional arguments
+                ;;
+            -s=*|--sep=*)
+                SEP="${arg#*=}"
+                shift
+                ;;
+            -i|--include-sep)
+                INCLUDE_SEP=true
+                shift
+                ;;
+            *)
+                OTHER_ARGUMENTS+=("$1")
+        esac
+    done
+}
 
+delete_old_log() {
+    if [[ -f  "$LOG_FILE" ]] ; then
+        rm "$LOG_FILE"
+    fi
+}
 
-if [[ -f  "$LOG_FILE" ]] ; then
-    rm "$LOG_FILE"
-fi
+print_affected_files() {
+    printf 'The following files will be processed:\n'
+    shopt -s nullglob
+    for f in *"$SEP"*; do
+        printf '%s\n' "$f"
+    done
+    shopt -u nullglob
+    printf '\n'
+}
 
+directory_name_from_filename() {
+    local filename=$1
 
-for f in *"$SEP"*; do
-  [[ -f "$f" ]] || continue # skip if not regular file
-  #dir="${f%.*}"
-  dir="${f%$SEP*}"
-  [[ $INCLUDE_SEP = true ]] && dir="$dir""$SEP";
-  if [[ $DRY_RUN = true ]]; then
-      echo "mv -i "'"'"$f"'"' '"'"$dir"'"'
-  else
-      printf "MOVE '%s'  TO '%s'\n" "$f" "$dir"
-      mkdir -p "$dir"
-      if [[ ! -d "$dir" ]]; then
-          printf "The target directory '%s' could not be created. Skipping to next file" "$dir"
-          continue # skip if no directory
-      fi
-      mv -i "$f" "$dir";
-      if [[ -f "$dir/$f" ]]; then
-          printf "Move successful\n"
-      else
-          printf "Error. File was not moved\n"
-          ERROR_OCURRED=true
-          printf "could not move %s" "$f" >> "$LOG_FILE"
-      fi
-  fi
-  printf '\n'
-done
+    local dir="${filename%$SEP*}"
+    [[ $INCLUDE_SEP = true ]] && dir="$dir""$SEP";
+    echo "$dir"
+}
 
-if [[ "$ERROR_OCURRED" = true ]]; then
-    printf "\nAN ERROR OCURRED, ONE OR MORE FILES COULD NOT BE MOVED! VIEW %s\n" "$LOG_FILE"
-fi
+create_target_directory() {
+    local dir=$1
+    mkdir -p "$dir"
+    if [[ ! -d "$dir" ]]; then
+        printf "The target directory '%s' could not be created. Skipping to next file" "$dir"
+        dir=''
+    fi
+    echo "$dir"
+}
 
-if [[ $DRY_RUN = true ]]; then
-    printf "\nThis was a DRY RUN\n"
-fi
+move_file() {
+    local file=$1
+    local dir=$2
+    local MV_ERROR=$(mv -i "$file" "$dir" 2>&1 /dev/null)
+    if [[ ! $MV_ERROR ]]; then
+        printf "Move successful\n"
+    else
+        printf "Error. File was not moved\n"
+        ERROR_OCURRED=true
+        printf "could not move '%s' : %s\n" "$f" "$MV_ERROR" >> "$LOG_FILE"
+    fi
+}
+
+print_error_notice() {
+    if [[ "$ERROR_OCURRED" = true ]]; then
+        printf "\nAN ERROR OCURRED, ONE OR MORE FILES COULD NOT BE MOVED! VIEW %s\n" "$LOG_FILE"
+    fi
+}
+
+print_dry_run_notice() {
+    if [[ $DRY_RUN = true ]]; then
+        printf "\nThis was a DRY RUN\n"
+    fi
+}
+
+main() {
+    parse_arguments $ARGS
+    print_affected_files
+    local f
+    for f in *"$SEP"*; do
+        [[ -f "$f" ]] \
+            || continue # skip if not regular file
+        dir=$(directory_name_from_filename "$f")
+        printf "MOVE '%s'  TO '%s'\n" "$f" "$dir"
+        [[ $DRY_RUN = true ]] \
+            && continue
+        delete_old_log
+        [[ $(create_target_directory "$dir") ]] \
+            || continue
+        move_file "$f" "$dir"
+        printf '\n'
+    done
+    print_error_notice
+    print_dry_run_notice
+}
+
+main
